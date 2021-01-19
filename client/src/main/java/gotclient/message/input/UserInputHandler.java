8ad08d876d3517bb9d0c537f038af8d93b3b45e8
Javier Gonzalez Diaz <javier.gonzales.diaz@careem.com>
2021-01-19 23:32:12 +0100

    Add further input validation to UserInputHandler to allow only input resulting in multiples of 3

--- client/src/main/java/gotclient/message/input/UserInputHandler.java
+++ client/src/main/java/gotclient/message/input/UserInputHandler.java
@@ -1,17 +1,16 @@
-package gotclient.message;
+package gotclient.message.input;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ClientRequestHandler {
+public class UserInputHandler {
 
   private final BufferedReader inputReader;
   private final Set<Integer> validGameNumbers = new HashSet<>();
 
-  public ClientRequestHandler(BufferedReader inputReader) {
+  public UserInputHandler(BufferedReader inputReader) {
     this.inputReader = inputReader;
 
     validGameNumbers.add(-1);
@@ -19,19 +18,20 @@ public class ClientRequestHandler {
     validGameNumbers.add(1);
   }
 
-  public int getValidNumber(boolean isFirstTurn) {
-    return isFirstTurn ? getStartingNumber() : getInputNumberFromSet(validGameNumbers);
+  public int getValidNumber(boolean isFirstTurn, int currentGameNumber) {
+    return isFirstTurn ? getStartingNumber() :
+      getInputNumberFromSet(validGameNumbers, currentGameNumber);
   }
 
   private int getStartingNumber() {
     int userInputNumber = Integer.MIN_VALUE;
     boolean validNumberEntered = false;
     while (!validNumberEntered) {
-      System.out.print("Enter a positive number: ");
+      System.out.print("Enter a positive number bigger than 3: ");
       try {
         userInputNumber = Integer.parseInt(inputReader.readLine());
-        if (userInputNumber <= 0) {
-          System.out.println("Only positive numbers are allowed!");
+        if (userInputNumber <= 3) {
+          System.out.println("Only positive numbers bigger than 3 are allowed!");
         } else {
           validNumberEntered = true;
         }
@@ -44,14 +44,22 @@ public class ClientRequestHandler {
     return userInputNumber;
   }
 
-  private int getInputNumberFromSet(Set<Integer> validNumbers) {
+  private int getInputNumberFromSet(Set<Integer> validNumbers, int currentGameNumber) {
     int userInputNumber = Integer.MIN_VALUE;
-    while (!validNumbers.contains(userInputNumber)) {
+    boolean isValidNumber = false;
+    while (!isValidNumber) {
       System.out.print("Enter a number from " + validNumbers + ": ");
       try {
         userInputNumber = Integer.parseInt(inputReader.readLine());
+        isValidNumber = true;
         if (!validNumbers.contains(userInputNumber)) {
           System.out.println("Only numbers in " + validNumbers + " are allowed!");
+          isValidNumber = false;
+        }
+        if ((currentGameNumber + userInputNumber) % 3 != 0) {
+          System.out.println(currentGameNumber + " + " + userInputNumber + " is not divisible by 3!");
+          System.out.println("Please try again");
+          isValidNumber = false;
         }
       } catch (NumberFormatException e) {
         System.out.println("Please enter a valid number!");
@@ -60,12 +68,5 @@ public class ClientRequestHandler {
       }
     }
     return userInputNumber;
-  }
-
-  public static void main(String[] args) {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    ClientRequestHandler req = new ClientRequestHandler(br);
-
-    System.out.println("Entered number: " + req.getValidNumber(true));
   }
 }
